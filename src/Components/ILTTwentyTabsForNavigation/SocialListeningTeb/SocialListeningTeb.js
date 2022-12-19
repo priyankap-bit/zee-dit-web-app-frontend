cca883347f83d15660805af1f342e23b47894d52
priyankap-bit <106512706+priyankap-bit@users.noreply.github.com>
2022-12-19 18:13:50 +0530

    Updated social-listening tab file

--- src/Components/ILTTwentyTabsForNavigation/SocialListeningTeb/SocialListeningTeb.js
+++ src/Components/ILTTwentyTabsForNavigation/SocialListeningTeb/SocialListeningTeb.js
@@ -5,21 +5,52 @@ import './SocialListeningTeb.css';
 const SocialListeningTeb = (props) => {
 
     return (      
-        <div className="executive-summary-tile">
-          <div className="executive-summary-tile-name">
-            <h4>Reach</h4>
+        <div className="social-listening-tile">
+          <div className="social-listening-title">
+            <h4>Total Mentions</h4>
+            <div className="executive-summary-tile-primary-val">
+                <h2>5.632 M</h2>
+              </div>
+              <div className="executive-summary-tile-secondary-val">
+                <p className="executive-summary-tile-secondary-val-perneg">
+                  -39.9%
+                </p>
+                <p className="executive-summary-tile-secondary-val-num">
+                  Prev. 9.37K
+                </p>
+              </div>
           </div>
-          <div className="executive-summary-tile-primary-val">
-            <h2>5.632 M</h2>
+          <div className="social-listening-title">
+            <h4>Total Distinct Users</h4>
+            <div className="executive-summary-tile-primary-val">
+                <h2>5.632 M</h2>
+              </div>
+              <div className="executive-summary-tile-secondary-val">
+                <p className="executive-summary-tile-secondary-val-perneg">
+                  -39.9%
+                </p>
+                <p className="executive-summary-tile-secondary-val-num">
+                  Prev. 9.37K
+                </p>
+              </div>
           </div>
-          <div className="executive-summary-tile-secondary-val">
-            <p className="executive-summary-tile-secondary-val-perneg">
-              -39.9%
-            </p>
-            <p className="executive-summary-tile-secondary-val-num">
-              Prev. 9.37K
-            </p>
+          <div className="social-listening-title">
+            <h4>Total Engagement</h4>
+            <div className="executive-summary-tile-primary-val">
+                <h2>5.632 M</h2>
+              </div>
+              <div className="executive-summary-tile-secondary-val">
+                <p className="executive-summary-tile-secondary-val-perneg">
+                  -39.9%
+                </p>
+                <p className="executive-summary-tile-secondary-val-num">
+                  Prev. 9.37K
+                </p>
+              </div>
           </div>
+         
+          
+          
         </div>
 
     )
