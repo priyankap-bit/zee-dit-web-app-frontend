0a002e6788e7b3e94ba12101e6cf2388ffd527a1
priyankap-bit <106512706+priyankap-bit@users.noreply.github.com>
2022-12-29 19:17:10 +0530

    icons added

--- src/Components/dataTowersContain/ILTTdataTowersContain.js
+++ src/Components/dataTowersContain/ILTTdataTowersContain.js
@@ -15,26 +15,26 @@ const ILTTdataTowersContain = (props) => {
            </div>
            <div className='data-towers'>
           <div className='Executive-Summary-tile'>
-              <img src='/static/images/logos/Executive-summary-icon.png' height={20} alt='Viewers Logo' className='icon' />
+              <img src='/static/images/logos/Consumer-icon.png' height={20} alt='Viewers Logo' className='icon' />
                <div className="tile-name">Consumer</div>
            </div>
           
            <div className='Executive-Summary-tile'>
-              <img src='/static/images/logos/Executive-summary-icon.png' height={20} alt='Viewers Logo' className='icon' />
+              <img src='/static/images/logos/Content-icon.png' height={20} alt='Viewers Logo' className='icon' />
                <div className="tile-name">Content</div>
            </div>
            <div className='Executive-Summary-tile'>
-              <img src='/static/images/logos/Executive-summary-icon.png' height={20} alt='Viewers Logo' className='icon' />
+              <img src='/static/images/logos/Customer-icon.png' height={20} alt='Viewers Logo' className='icon' />
                <div className="tile-name">Customer</div>
            </div>
            <div className='Executive-Summary-tile'>
-              <img src='/static/images/logos/Executive-summary-icon.png' height={20} alt='Viewers Logo' className='icon' />
+              <img src='/static/images/logos/Product-icon.png' height={20} alt='Viewers Logo' className='icon' />
                <div className="tile-name">Product</div>
            </div>
            </div>
            <div className='Executive-Summary-tile'>
-              <img src='/static/images/logos/Executive-summary-icon.png' height={20} alt='Viewers Logo' className='icon' />
-               <a href='/ilt20-summary'><div className="tile-name">Sports</div></a>
+              <img src='/static/images/logos/Sports-icon.png' height={20} alt='Viewers Logo' className='icon' />
+               <a href='/ilt20-summary' className='sports'><div className="tile-name">Sports</div></a>
            </div>
         </div> 
     
