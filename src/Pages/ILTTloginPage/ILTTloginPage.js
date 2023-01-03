67b3a2dc61013cd22912d57873d6bfe38562070f
priyankap-bit <106512706+priyankap-bit@users.noreply.github.com>
2023-01-03 14:49:42 +0530

    Login page css has been applied.

--- src/Pages/ILTTloginPage/ILTTloginPage.js
+++ src/Pages/ILTTloginPage/ILTTloginPage.js
@@ -11,10 +11,13 @@ const ILTTloginPage = (props) => {
                 <img src='/static/images/logos/Branding.png' height={70} alt='Viewers Logo' className='branding-image' />
             </div>
             <form className='login-page'>
-                <input type="email" name="name" placeholder='Email' /> <br />
-                <input type="password" name="pass" placeholder='Password' /> <br />
+                <img src='/static/images/logos/login-email.svg' alt='Viewers Logo' className='email-image' />
+                <input type="email" name="name" id="email" placeholder='Email' /> <br />
+                <img src='/static/images/logos/password.svg' alt='Viewers Logo' className='password-image' />
+                <input type="password" name="pass" id="pass" placeholder='Password' /> <br />
+                <p className='forgot-password'>Forgot password?</p>
                 <a href="/" className="login">LOGIN</a>
-                {/* <input className='login' type="submit" value="LOGIN" /> */}
+                
             </form>
         </div>
 
