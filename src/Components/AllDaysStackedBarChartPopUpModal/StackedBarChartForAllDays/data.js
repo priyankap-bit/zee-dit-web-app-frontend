970e6a282bafa8128aaa2956a52eab9776addd94
Miloni Shah <vc.miloni.shah@zee.com>
2023-01-05 18:28:49 +0530

    Added stacked bar chart popup.

---
+++ src/Components/AllDaysStackedBarChartPopUpModal/StackedBarChartForAllDays/data.js
@@ -0,0 +1,188 @@
+const data = [
+    {
+        key: "Jan1'23",
+        matchOne: 50,
+        matchTwo: 70,
+    },
+    {
+        key: "2",
+        matchOne: 40,
+        matchTwo: 60,
+    },
+    {
+        key: "3",
+        matchOne: 30,
+        matchTwo: 50,
+    },
+    {
+        key: "4",
+        matchOne: 45,
+        matchTwo: 55,
+    },
+    {
+        key: "5",
+        matchOne: 25,
+        matchTwo: 40,
+    },
+    {
+        key: "6",
+        matchOne: 35,
+        matchTwo: 50,
+    },
+    {
+        key: "7",
+        matchOne: 40,
+        matchTwo: 55,
+    },
+    {
+        key: "8",
+        matchOne: 50,
+        matchTwo: 70,
+    },
+    {
+        key: "9",
+        matchOne: 40,
+        matchTwo: 60,
+    },
+    {
+        key: "10",
+        matchOne: 30,
+        matchTwo: 50,
+    },
+    {
+        key: "11",
+        matchOne: 45,
+        matchTwo: 55,
+    },
+    {
+        key: "12",
+        matchOne: 25,
+        matchTwo: 40,
+    },
+    {
+        key: "13",
+        matchOne: 35,
+        matchTwo: 50,
+    },
+    {
+        key: "14",
+        matchOne: 40,
+        matchTwo: 55,
+    },
+    {
+        key: "15",
+        matchOne: 50,
+        matchTwo: 70,
+    },
+    {
+        key: "16",
+        matchOne: 40,
+        matchTwo: 60,
+    },
+    {
+        key: "17",
+        matchOne: 30,
+        matchTwo: 50,
+    },
+    {
+        key: "18",
+        matchOne: 45,
+        matchTwo: 55,
+    },
+    {
+        key: "19",
+        matchOne: 25,
+        matchTwo: 40,
+    },
+    {
+        key: "20",
+        matchOne: 35,
+        matchTwo: 50,
+    },
+    {
+        key: "21",
+        matchOne: 40,
+        matchTwo: 55,
+    },
+    {
+        key: "22",
+        matchOne: 50,
+        matchTwo: 70,
+    },
+    {
+        key: "23",
+        matchOne: 40,
+        matchTwo: 60,
+    },
+    {
+        key: "24",
+        matchOne: 30,
+        matchTwo: 50,
+    },
+    {
+        key: "25",
+        matchOne: 45,
+        matchTwo: 55,
+    },
+    {
+        key: "26",
+        matchOne: 25,
+        matchTwo: 40,
+    },
+    {
+        key: "27",
+        matchOne: 35,
+        matchTwo: 50,
+    },
+    {
+        key: "28",
+        matchOne: 40,
+        matchTwo: 55,
+    },
+    {
+        key: "29",
+        matchOne: 50,
+        matchTwo: 70,
+    },
+    {
+        key: "30",
+        matchOne: 40,
+        matchTwo: 60,
+    },
+    {
+        key: "31",
+        matchOne: 30,
+        matchTwo: 50,
+    },
+    {
+        key: "Feb'1",
+        matchOne: 45,
+        matchTwo: 55,
+    },
+    {
+        key: "2",
+        matchOne: 25,
+        matchTwo: 40,
+    },
+    {
+        key: "3",
+        matchOne: 35,
+        matchTwo: 50,
+    },
+]
+
+const keys = [
+    "matchOne",
+    "matchTwo",
+]
+
+const colors = {
+    matchOne: "#945ED2",
+    matchTwo: "#C996EB",
+}
+
+export {
+    data,
+    keys,
+    colors,
+}
