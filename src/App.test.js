91527424175a8ae82128c5a4d2ca54fd0df89753
priyanka <vc.miloni.shah@zee.com>
2023-01-06 10:56:09 +0530

    Figma link

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,5 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+https://www.figma.com/proto/huy10ZW73MT6MF1IqeR7Zh/Data-intelligence-CT?page-id=0%3A1&node-id=1345%3A5952&viewport=-956%2C-6672%2C0.45&scaling=contain&starting-point-node-id=790%3A1310
