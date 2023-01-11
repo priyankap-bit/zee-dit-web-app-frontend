2a73ac5d1091ade3e99339c9b8e7b8fa69a48e43
milonishah9 <107255075+milonishah9@users.noreply.github.com>
2023-01-11 16:09:31 +0530

    Create product tab

---
+++ src/Components/ExecutiveSummaryTab/ExecutiveSummaryProduct/ExecutiveSummaryProduct.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ExecutiveSummaryContent.css"
+import { useState } from "react";
+import BubbleChart from "../../Charts/BubbleChart/BubbleChart";
+import ExecutiveSummaryContentLinear from "./ExecutiveSummaryContentLinear";
+import ExecutiveSummaryContentOtt from "./ExecutiveSummaryContentOtt";
+import LinearBarchart from "../../Charts/LinearBarchart/LinearBarchart";
+
+const ExecutiveSummaryContent = () => {
+    const [activeTab, setActiveTab] = useState('linear');
+
+    return (
+        <div>
+            <div className="executive-summary-content-tabs">
+                <div onClick={() => setActiveTab('linear')} className={activeTab === 'linear' ? ("executive-summary-content-active-tab") : ("executive-summary-content-tab")}>Linear</div>
+                <div onClick={() => setActiveTab('ott')} className={activeTab === 'ott' ? ("executive-summary-content-active-tab") : ("executive-summary-content-tab")}>OTT</div>
+            </div>
+
+            <div>
+                {activeTab === 'linear' && <ExecutiveSummaryContentLinear /> }
+                {activeTab === 'linear' &&  <LinearBarchart/> }
+                {activeTab === 'ott' && <ExecutiveSummaryContentOtt />}
+            </div>
+        </div>
+    )
+}
+
+export default ExecutiveSummaryContent;
