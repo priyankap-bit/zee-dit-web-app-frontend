6f28ba719a1f9b6bb6d7dc1d9bbaf7081f63b4ac
Miloni Shah <vc.miloni.shah@zee.com>
2023-01-13 11:16:55 +0530

    Updated raw data for line chart.

--- src/Components/Charts/LineChartWithToolTip/data.js
+++ src/Components/Charts/LineChartWithToolTip/data.js
@@ -2,31 +2,31 @@
 const lineChartWithToolTipsData = [
     {
         date: "1900-01-01T00:00:00.000Z",
-        population: 6094000
+        population: 6094
     },
     {
         date: "1901-01-01T00:00:00.000Z",
-        population: 37584000
+        population: 37584
     },
     {
         date: "1902-01-01T00:00:00.000Z",
-        population: 79163000
+        population: 79163
     },
     {
         date: "1903-01-01T00:00:00.000Z",
-        population: 40632000
+        population: 40632
     },
     {
         date: "1904-01-01T00:00:00.000Z",
-        population: 62166000
+        population: 62166
     },
     {
         date: "1905-01-01T00:00:00.000Z",
-        population: 13822000
+        population: 13822
     },
     {
         date: "1906-01-01T00:00:00.000Z",
-        population: 65450000
+        population: 65450
     },
 ]
 
