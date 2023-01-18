fd8a98bdc8cd574b17dda5dc1e75c990eb11def1
priyankap-bit <106512706+priyankap-bit@users.noreply.github.com>
2023-01-18 11:15:32 +0530

    Update BubbleChart.js

--- src/Components/Charts/BubbleChart/BubbleChart.js
+++ src/Components/Charts/BubbleChart/BubbleChart.js
@@ -1,235 +1,270 @@
 import React from "react";
-import * as d3 from 'd3';
-import './BubbleChart.css'
+import "./BubbleChart.css";
+import * as d3 from "d3v4";
 import { useEffect } from "react";
-import { drag, dragDisable, dragEnable, forceY, scaleLinear, select, ticks } from "d3";
-const BubbleChart = (props) => {
-    const { files } = props
-    const width = window.innerWidth;
-    useEffect(() => {
-        const chart = BubbleChart(files, {
-            label: d => [...d.id.split(".").pop().split(/(?=[A-Z][a-z])/g), d.value].join("\n"),
-            value: d => d.value,
-            group: d => d.id.split(".")[1],
-            title: d => `${d.id}\n${d.value}`,
-            width: width
-            // link: d => `https://github.com/prefuse/Flare/blob/master/flare/src/${d.id.replace(/\./g, "/")}.as`,
-        })
-    }, [files])
+import { select } from "d3";
 
-    // Copyright 2021 Observable, Inc.
-    // Released under the ISC license.
-    // https://observablehq.com/@d3/bubble-chart
-    function BubbleChart(data, {
-        name = ([x]) => x, // alias for label
-        label = name, // given d in data, returns text to display on the bubble
-        value = ([, y]) => y, // given d in data, returns a quantitative size
-        group, // given d in data, returns a categorical value for color
-        title, // given d in data, returns text to show on hover
-        link, // given a node d, its link (if any)
-        linkTarget = "https://themepack.me/i/c/749x467/media/g/2137/dark-galaxy-theme-jy8.jpg", // the target attribute for links, if any
-        width = width, // outer width, in pixels
-        height = 500, // outer height, in pixels
-        padding = 0, // padding between circles
-        margin = 1, // default margins
-        marginTop = margin, // top margin, in pixels
-        marginRight = margin, // right margin, in pixels
-        marginBottom = margin, // bottom margin, in pixels
-        marginLeft = margin, // left margin, in pixels
-        groups, // array of group names (the domain of the color scale)
-        colors = d3.schemeTableau10, // an array of colors (for groups)
-        fill = "#ccc", // a static fill color, if no group channel is specified
-        fillOpacity = 0.7, // the fill opacity of the bubbles
-        stroke, // a static stroke around the bubbles
-        strokeWidth, // the stroke width around the bubbles, if any
-        strokeOpacity, // the stroke opacity around the bubbles, if any
-    } = {}) {
-        // Compute the values.
-        const D = d3.map(data, d => d);
-        const V = d3.map(data, value);
-        const G = group == null ? null : d3.map(data, group);
-        const I = d3.range(V.length).filter(i => V[i] > 0);
-        // Unique the groups.
-        if (G && groups === undefined) groups = I.map(i => G[i]);
-        groups = G && new d3.InternSet(groups);
+const BubbleChart2 = () => {
+  var json = [
+    { Name: "Accounting Services", Count: 7 },
+    { Name: "Architectural Consulting Services", Count: 1 },
+    { Name: "Commercial/Industrial Real Estate Brokerage Services", Count: 2 },
+    { Name: "Construction/Engineering Consulting Services", Count: 2 },
+    { Name: "Coworking Space", Count: 1 },
+    { Name: "Customs Brokerage Services", Count: 3 },
+    { Name: "Energy Audit or Consulting Services", Count: 4 },
+    { Name: "E", Count: 4 },
+    {
+      Name: "Human Resources and/or Executive Talent Search Services",
+      Count: 4,
+    },
+    { Name: "Insurance Brokerage Services", Count: 4 },
+    { Name: "Legal Services", Count: 11 },
+    { Name: "Managed IT Services", Count: 4 },
+    { Name: "Marketing and Communications Services", Count: 6 },
+    { Name: "Relocation Specialists", Count: 1 },
+  ];
 
-        // Construct scales.
-        // const color = G && d3.scaleOrdinal(groups, colors);
+  useEffect(() => {
+    // d3 colour scheme for bubbles
+    // var color = d3.scaleOrdinal().range(["rgb(148, 94, 210, 0.1)"]);
 
-        // Compute labels and titles.
-        const L = label == null ? null : d3.map(data, label);
-        const T = title === undefined ? L : title == null ? null : d3.map(data, title);
+    // tooltip with styling
+    var tooltip = d3
+      .select("#chart")
+      .append("div")
+      .style("position", "absolute")
+      .style("z-index", "10")
+      .style("visibility", "hidden")
+      .style("color", "black")
+      .style("padding", "8px")
+      .style("background-color", "white")
+      .style("border-radius", "4px")
+      .style("font", "12px GothamLight")
+      .text("tooltip");
 
-        const root = d3.pack()
-            .size([width - marginLeft - marginRight, height - marginTop - marginBottom])
-            .padding(padding)
-            (d3.hierarchy({ children: I })
-                .sum(i => V[i]));
+    // fit bubble chart to the user's window
+    var margin = { left: 10, right: 10, top: 10, bottom: 10 },
+      width = 850,
+      height = 350,
+      svg = d3
+        .select("#chart")
+        .append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 850 200")
+        .classed("svg-content", true)
+        .append("g");
 
-        const svg = d3.select("#bubbleChart")
-            .attr("width", width)
-            .attr("height", height)
-            .attr("viewBox", [-marginLeft, -marginTop, width, height])
-            .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
-            .attr("fill", "currentColor")
-            .attr("font-size", 10)
-            .attr("font-family", "GothamLight")
-            .attr("text-anchor", "middle")
+    // var button = d3
+    //   .select("#chart")
+    //   .append("button")
+    //   .attr("id", "btn1")
+    //   .attr("class", "btn1")
+    //   .text("Show Legend")
+    //   .on("click", myFunction());
 
-        const leaf = svg.selectAll("a")
-            .data(root.leaves())
-            .join("a")
-            .attr("xlink:href", link == null ? null : (d, i) => link(D[d.data], i, data))
-            .attr("target", link == null ? null : linkTarget)
+    var simulation = d3
+      .forceSimulation()
+      .velocityDecay(0.1)
+      .force("x", d3.forceX(width / 2).strength(0.005))
+      .force("y", d3.forceY(height / 3.5).strength(0.09))
+      .force(
+        "collide",
+        d3
+          .forceCollide(function (d) {
+            return d.Count * 7 + 3;
+          })
+          .iterations(10)
+      );
 
-        const trans = leaf
-            .attr("transform", 'translate(770, 200)')
-        // .attr("transform", d => `translate(${d.x}, 200)`)
+    var circles = svg
+      .selectAll(".artist")
+      .data(json)
+      .enter()
+      .append("circle")
+      .attr("stroke", "#945ED2")
+      .attr("stroke-width", 1)
+      .attr("stroke-opacity", 2)
+      .attr("fill", "rgb(148, 94, 210, 0.1)")
+      .attr("class", "artist")
+      .attr("fill", "rgb(148, 94, 210, 0.1)")
+      .attr("r", function (d) {
+        return d.Count * 7;
+      })
+      //   .style("fill", function (d, i) {
+      //     var bubbleColor = color(d.Name);
+      //       select("#bubble-legend").append(
+      //         '<li><span class="bubble-legend-box" style="background:' +
+      //           bubbleColor +
+      //           '"></span><span class="accessible">' +
+      //           d.Count +
+      //           '</span><span class="bubble-legend-label" >' +
+      //           d.Name +
+      //           "</span></li>"
+      //       );
+      //     return bubbleColor;
+      //   })
 
-        trans.transition()
-            .duration(1000)
-            .attr("transform", d => `translate(${d.x},${d.y})`)
+    //   .attr("color", function (d) {
+    //     return d3.select(this).style("fill");
+    //   })
+      .call(
+        d3
+          .drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended)
+      )
+      .on("mouseover", function (d) {
+        //tooltips
+        tooltip.text(d.Name + ": " + d.Count);
+        tooltip.style("visibility", "visible");
+      })
+      .on("mousemove", function () {
+        return tooltip
+          .style("top", d3.event.pageY - 10 + "px")
+          .style("left", d3.event.pageX + 10 + "px");
+      })
+      .on("mouseout", function () {
+        return tooltip.style("visibility", "hidden");
+      });
 
-
-
-
-        // -------------------new code -----------------------------------
-        var simulation = d3
-            .forceSimulation(files)
-            .velocityDecay(0.005)
-            .velocityDecay(0.1)
-            .force("x", d3.forceX(width / 4).strength(0.00005))
-            .force("y", d3.forceY(height).strength(9))
-            .force(
-                "collide",
-                d3
-                    .forceCollide(function (d) {
-                        return d.Count * 7 + 3;
-                    })
-                    .iterations(10)
-            );
-        // ------------------------------------------------------------------------------------
-
-
-
-
-
-
-        const circle = leaf.append("circle")
-            .attr("stroke", '#945ED2')
-            .attr("stroke-width", strokeWidth)
-            .attr("stroke-opacity", strokeOpacity)
-            .attr("fill", 'rgb(148, 94, 210, 0.1)')
-            .attr("fill-opacity", fillOpacity)
-            .attr("r", d => d.r)
-            .style("cursor", "pointer")
-            .on('click', function (d, i) {
-                d3.select(this)
-                    .attr("stroke-width", 5)
-                // .transition()
-                // .duration(1000)
-                // .ease(d3.easeBounce)
-                // .attr("r", 50)
-                // .style("fill", "orange");
-
-                // leaf.filter(function (e) {
-                //     return e.rank === d.rank;
-                // }).transition()
-                //     .duration(1000)
-                //     .ease(d3.easeBounce)    
-                //     .attr("font-size", "10px")
-            })
-            .on('mouseout', function (d, i) {
-                d3.select(this)
-                    .attr("stroke-width", strokeWidth)
-                // .transition()
-                //     .style("opacity", 0.3)
-                //     .attr("r", 50)
-                //     .style("fill", "blue");
-                // leaf.filter(function (e) {
-                //     return e.rank === d.rank;
-                // }).attr("font-size", "10px")
-            })
-
-        if (T) leaf.append("title")
-            .attr("class", "tooltip-title")
-            .text(d => T[d.data])
-        // .style("stroke", "black")
-        // .attr('font-size', '15px')
-
-        if (L) {
-            // A unique identifier for clip paths (to avoid conflicts).
-            const uid = `O-${Math.random().toString(16).slice(2)}`;
-
-            leaf.append("clipPath")
-                .attr("id", d => `${uid}-clip-${d.data}`)
-                .append("circle")
-                .attr("r", d => d.r);
-
-            const text = leaf.append("text")
-                //   .attr("clip-path", d => `url(${new URL(`#${uid}-clip-${d.data}`, location)})`)
-                .selectAll("tspan")
-                .data(d => `${L[d.data]}`.split(/\n/g))
-                .join("tspan")
-                .style("cursor", "pointer")
-                .attr("x", 0)
-                .attr("y", (d, i, D) => `${i - D.length / 2 + 0.85}em`)
-                .attr("font-family", (d, i, D) => i === D.length - 1 ? (d) : null)
-                .text((d, i, D) => i === D.length - 1 ? (Number(d) < 300 ? (d3.select('text').attr('class', 'lessthan300')) : d) : d)
-
-
-            // ---------------------------new code -----------------------------------------
-            simulation.nodes(D).on("tick", ticked);
-
-            function ticked() {
-                circle
-                    .attr("x", function (d) {
-                        return d.x;
-                    })
-                    .attr("y", function (d) {
-                        return d.y;
-                    })
-                // text
-                // .attr("x", 0 )
-                // // .attr("y", (d, i, D) => `${i - D.length / 2 + 0.85}em`)
-                // .attr("font-family", (d, i, D) => i === D.length - 1 ? 'GothamBold' : null)
-
-            }
-            // -----------------------------------------------------------------------
-
-
-
+    function wrap(text, width) {
+      text.each(function () {
+        var text = d3.select(this),
+          words = text.text().split(" ", 3).reverse(),
+          word,
+          line = [],
+          lineNumber = 0,
+          lineHeight = 0, // ems
+          y = text.attr("y"),
+          dy = parseFloat(text.attr("dy")),
+          tspan = text.text(null).append("tspan");
+        while ((word = words.pop())) {
+          line.push(word);
+          tspan.text(line.join(" "));
+          if (tspan.node().getComputedTextLength() > width) {
+            line.pop();
+            tspan.text(line.join(" "));
+            line = [word];
+            tspan = text
+              .append("tspan")
+              .attr("dx", "-6.5em")
+              .attr("dy", lineNumber * lineHeight + dy + "em")
+              .text(word);
+          }
         }
-
-
-
-
-
-        // return Object.assign(svg.node(), { scales: { color } });
+      });
     }
 
-    return (
-        <div className="bubble-chart-div">
-            <div className="bubble-indicators">
-                Bubble size represents Reach
-                <div className="indicators">
-                    <img src="/static/images/logos/xxk.svg" className="xxk" alt="Viewers Logo" />
-                    xxk
-                    <img src="/static/images/logos/xxk-2.svg" className="xxk" alt="Viewers Logo" />
-                    xxk
-                    <img src="/static/images/logos/xxk-3.svg" className="xxk" alt="Viewers Logo" />
-                    xxk
-                    <img src="/static/images/logos/xxk-4.svg" className="xxk" alt="Viewers Logo" />
-                    xxk
-                </div>
+    //text for name in bubble
+    var textName = svg
+      .selectAll(null)
+      .data(json)
+      .enter()
+      .append("text")
+      .style("text-anchor", "middle")
+      .attr("dy", "1.5em")
+      .text(function (d) {
+        return d.Count > 4 ? d.Name : " ";
+      })
+      .call(wrap, 80);
 
-            </div>
-            <svg id='bubbleChart'></svg>
-            <div>
+    //text for count in bubble
+    var textCount = svg
+      .selectAll(null)
+      .data(json)
+      .enter()
+      .append("text")
+      .text(function (d) {
+        return d.Count > 4 ? d.Count : "";
+      })
+      .attr("dy", "0.1em")
+      .style("text-anchor", "middle")
+      .attr("class", "service-title");
 
-            </div>
+    simulation.nodes(json).on("tick", ticked);
+
+    function ticked() {
+      circles
+        .attr("cx", function (d) {
+          return d.x;
+        })
+        .attr("cy", function (d) {
+          return d.y;
+        });
+      textName
+        .attr("x", function (d) {
+          return d.x;
+        })
+        .attr("y", function (d) {
+          return d.y;
+        });
+      textCount
+        .attr("x", function (d) {
+          return d.x;
+        })
+        .attr("y", function (d) {
+          return d.y;
+        });
+    }
+
+    function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = null;
+      d.fy = null;
+    }
+
+    function myFunction() {
+      // $("#btn1").on("click", function() {
+      //   if ($("#bubble-legend-container").css("display") == "none") {
+      //     //$("#bubble-legend").css("display","block");
+      //     $("#bubble-legend-container").slideDown("slow");
+      //     $("#btn1").text("Hide Legend");
+      //   } else {
+      //     $("#bubble-legend-container").slideUp("slow");
+      //     // $("#bubble-legend").css("display","none");
+      //     $("#btn1").text("Show Legend");
+      //   }
+      // });
+    }
+  }, [json]);
+
+  return (
+    <>
+      {/* <div className="article">
+        <div className="container-12">
+          <div className="grid-12">
+            <h2>
+              <strong>54</strong> Companies in <strong>14</strong> Service
+              Categories
+            </h2>
+          </div>
         </div>
-    )
-}
+      </div> */}
 
-export default BubbleChart;
+      <div id="chart" className="chart"></div>
+
+      {/* <div className="article" id="bubble-legend-container">
+        <div className="container-12">
+          <div className="grid-12">
+            <ul id="bubble-legend"></ul>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
+};
+
+export default BubbleChart2;
